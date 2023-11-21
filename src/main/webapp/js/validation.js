4fc5c693391b20c1ebc0f4f19c55cfe26eda926b
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-11-21 21:16:38 +0300

    Added main.xhtml page

---
+++ src/main/webapp/js/validation.js
@@ -0,0 +1,16 @@
+function buttonsInit() {
+    for (let i = 1; i <= 5; i++) {
+        document.getElementById("form:r" + i).addEventListener("click", () => {
+            document.getElementById("coorRd2_x").innerHTML = String(i / 2)
+            document.getElementById("coorR_x").innerHTML = String(i)
+            document.getElementById("coorRd2_y").innerHTML = String(i / 2)
+            document.getElementById("coorR_y").innerHTML = String(i)
+            document.getElementById("coor-Rd2_x").innerHTML = String(i / 2)
+            document.getElementById("coor-R_x").innerHTML = String(i)
+            document.getElementById("coor-Rd2_y").innerHTML = String(i / 2)
+            document.getElementById("coor-R_y").innerHTML = String(i)
+        })
+    }
+}
+
+buttonsInit()
