b54562ffbf9415e4e8d71754bf0da8876d37e552
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-12-02 10:22:40 +0300

    Added my implementation of @Inject

--- src/main/java/org/example/dependency/InjectionFactory.java
+++ src/main/java/org/example/dependency/InjectionFactory.java
@@ -10,10 +10,10 @@ public class InjectionFactory {
         Field[] fields = object.getClass().getDeclaredFields();
         for (Field field: fields) {
             Class<?> fieldType = field.getType();
-            if(!fieldType.isAnnotationPresent(Named.class))
+            if(!fieldType.isAnnotationPresent(Named.class))     //если поле - не bean, то просто пропускаю это поле
                 continue;
             String beanName = fieldType.getAnnotation(Named.class).value();
-            if (beanName.equals("")) {
+            if (beanName.equals("")) {      //если я не указывал значение Named, то меняю первую букву класса на ту же букву в нижнем регистре
                 String className = fieldType.getSimpleName();
                 beanName = Character.toLowerCase(className.charAt(0)) + className.substring(1);
             }
@@ -22,7 +22,7 @@ public class InjectionFactory {
             try {
                 field.setAccessible(true);
                 Object o = context.getApplication().evaluateExpressionGet(context, "#{" + beanName + "}", fieldType);
-
+                //из контекста faces получаю созданный jsf'ом объект бина и устанавливаю его в поле переданного в метод объекта, таким образом инициализирую поле
                 field.set(object, o);
 
             } catch (IllegalAccessException e) {
